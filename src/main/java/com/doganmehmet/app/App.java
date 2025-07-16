5c5b21d8a1615614f2b5ee1b2bcad7355dcd3f60
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2025-07-16 16:49:06 +0200

    Initial project setup
    - Initialized Spring Boot project with required dependencies
    - Configured application.properties files

--- src/main/java/com/doganmehmet/app/App.java
+++ src/main/java/com/doganmehmet/app/App.java
@@ -2,8 +2,14 @@ package com.doganmehmet.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.doganmehmet")
+@EnableJpaRepositories(basePackages = "com.doganmehmet")
+@EntityScan(basePackages = "com.doganmehmet")
 public class App {
 	public static void main(String[] args)
 	{
