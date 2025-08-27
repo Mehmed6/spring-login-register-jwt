bae1165eb9ba534af32dab47e5968cb62e083ae5
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2025-08-28 00:58:25 +0200

    update

--- src/main/java/com/doganmehmet/app/controller/LoginController.java
+++ src/main/java/com/doganmehmet/app/controller/LoginController.java
@@ -6,11 +6,9 @@ import com.doganmehmet.app.service.LoginService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth/login")
